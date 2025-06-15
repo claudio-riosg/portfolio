1abf2aa934a0dc0dd6ca32fc822135f1f75be5cc
Claudio <claudio@Mac.lan>
2025-06-15 02:47:41 -0600

    update icons in my-approach component

--- src/app/features/home/components/my-approach/my-approach.component.ts
+++ src/app/features/home/components/my-approach/my-approach.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IconComponent } from '../../../../shared/ui/svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-my-approach',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, IconComponent],
   templateUrl: './my-approach.component.html',
   styleUrl: './my-approach.component.scss',
 })
